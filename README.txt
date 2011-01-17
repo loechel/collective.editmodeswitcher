48b5e70cce4bb273af07e100d36cac564b4a09af
Thomas Buchberger <t.buchberger@4teamwork.ch>
2011-01-17 09:58:40 +0100

    Created product.

---
+++ README.txt
@@ -0,0 +1,10 @@
+Introduction
+============
+
+collective.editmodeswitcher allows to toggle between edit and view only mode.
+This enables users with modify permissions to view a page without editable
+borders. The current setting is stored in a cookie and thus effective for 
+multiple page views. This is especially useful in single sign on environments.
+
+
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.editmodeswitcher',
+      version=version,
+      description="Allows to toggle edit mode in Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Thomas Buchberger',
+      author_email='t.buchberger@4teamwork.ch',
+      url='http://www.4teamwork.ch',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
